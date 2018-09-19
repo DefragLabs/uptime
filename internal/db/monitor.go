cace3af05e5a88098e3ca97a715aa18d2d17e909
dinesh <dineshpy07@gmail.com>
2018-09-20 00:24:25 +0530

    Don't send ID to mongo db. That will be autogenerated.

--- internal/db/monitor.go
+++ internal/db/monitor.go
@@ -1,12 +1,10 @@
 package db
 
-import (
-	"github.com/mongodb/mongo-go-driver/bson/objectid"
-)
+import "github.com/mongodb/mongo-go-driver/bson/objectid"
 
 // MonitorURL struct represents a row in db.
 type MonitorURL struct {
-	ID        objectid.ObjectID `bson:"_id" json:"id"`
+	ID        objectid.ObjectID `bson:"-,Skip" json:"id,omitempty"`
 	Protocol  string            `bson:"protocol" json:"protocol"`
 	URL       string            `bson:"url" json:"url"`
 	frequency int32             `bson:"frequency" json:"frequency"`
--- internal/db/notification.go
+++ internal/db/notification.go
@@ -6,7 +6,7 @@ import (
 
 // Notification struct represents a row in db.
 type Notification struct {
-	ID         objectid.ObjectID `bson:"_id" json:"id"`
+	ID         objectid.ObjectID `bson:"-,Skip" json:"id,omitempty"`
 	Type       string            `bson:"type" json:"type"`
 	EmailID    string            `bson:"emailID" json:"emailID"`
 	webhookURL string            `bson:"webhookURL" json:"webhookURL"`
