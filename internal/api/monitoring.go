2ddeb1183a7f0eaf4d1e2934c871d4f33721a96c
dinesh <dineshpy07@gmail.com>
2018-09-19 21:51:18 +0530

    Add skeleton for monitoring api's

---
+++ internal/api/monitoring.go
@@ -0,0 +1,16 @@
+package api
+
+import "net/http"
+
+// addMonitoringDetails api lets an user add an healthcheck url.
+func addMonitoringDetails(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getMonitoringDetails(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateMonitoringDetails(w http.ResponseWriter, r *http.Request) {
+
+}
