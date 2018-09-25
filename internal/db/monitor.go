7f1b1c59e97a58dab74657c1fecae66f1e320921
dinesh <dineshpy07@gmail.com>
2018-09-25 15:54:09 +0530

    Add monitor result struct

--- internal/db/monitor.go
+++ internal/db/monitor.go
@@ -2,9 +2,30 @@ package db
 
 // MonitorURL struct represents a row in db.
 type MonitorURL struct {
-	ID        string `bson:"_id" json:"id,omitempty"`
-	Protocol  string `bson:"protocol" json:"protocol"`
-	URL       string `bson:"url" json:"url"`
-	Frequency int32  `bson:"frequency" json:"frequency"`
-	Unit      string `bson:"unit" json:"unit"`
+	ID string `bson:"_id" json:"id,omitempty"`
+
+	// Http protocol (http/https)
+	Protocol string `bson:"protocol" json:"protocol"`
+
+	// URL that should be pinged.
+	URL string `bson:"url" json:"url"`
+
+	// Frequency in integer
+	Frequency int32 `bson:"frequency" json:"frequency"`
+
+	// Time unit (minutes, hours)
+	Unit    string           `bson:"unit" json:"unit"`
+	Results []*MonitorResult `bson:"results,omitempty" json:"results"`
+}
+
+// MonitorResult contains the ping result.
+type MonitorResult struct {
+	// Status code of the response.
+	Status int32
+
+	// Duration of response
+	Duration string
+
+	// Timestamp when the ping was run.
+	Time string
 }
