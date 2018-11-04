8d4241a5b27619eb39055d34028240b4f6abaf3b
dinesh <dineshpy07@gmail.com>
2018-11-04 23:53:21 +0530

    Add logrus

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"os"
+
 	"github.com/defraglabs/uptime/internal/api"
+	"github.com/defraglabs/uptime/internal/tasks"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	go uptime.StartScheduler()
+
+	setupLogin()
 	api.StartServer()
+}
+
+func setupLogin() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.WarnLevel)
 }
