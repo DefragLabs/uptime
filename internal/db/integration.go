ed6cd687c98f3cb9b94d1db7e5fd8f358d35da29
dinesh <dineshpy07@gmail.com>
2018-11-29 13:21:04 +0530

    Rename notification to integration

---
+++ internal/db/integration.go
@@ -0,0 +1,14 @@
+package db
+
+import (
+	"github.com/mongodb/mongo-go-driver/bson/objectid"
+)
+
+// Integration struct represents a row in db.
+type Integration struct {
+	ID         objectid.ObjectID `bson:"-,Skip" json:"id,omitempty"`
+	UserID     string            `bson:"userID" json:"userID"`
+	Type       string            `bson:"type" json:"type"`
+	EmailID    string            `bson:"emailID" json:"emailID"`
+	WebhookURL string            `bson:"webhookURL" json:"webhookURL"`
+}
