16d410f31e087cfd05b8acdd35e3ef518d46a941
dinesh <dineshpy07@gmail.com>
2018-12-03 13:06:39 +0530

    Update bson tag for db.Integration

--- internal/db/integration.go
+++ internal/db/integration.go
@@ -2,7 +2,7 @@ package db
 
 // Integration struct represents a row in db.
 type Integration struct {
-	ID         string `bson:"-,Skip" json:"id,omitempty"`
+	ID         string `bson:"_id" json:"id,omitempty"`
 	UserID     string `bson:"userID" json:"userID"`
 	Type       string `bson:"type" json:"type"`
 	Email      string `bson:"email" json:"email"`
