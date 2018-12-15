309fbb6d93eb8ca08f6ac01ed915e6ece5791247
dinesh <dineshpy07@gmail.com>
2018-12-15 22:35:20 +0530

    Add pagerduty integration

--- internal/db/integration.go
+++ internal/db/integration.go
@@ -1,10 +1,58 @@
 package db
 
+import (
+	"errors"
+
+	"github.com/PagerDuty/go-pagerduty"
+	log "github.com/sirupsen/logrus"
+)
+
 // Integration struct represents a row in db.
 type Integration struct {
-	ID         string `bson:"_id" json:"id,omitempty" structs:"id"`
-	UserID     string `bson:"userID" json:"userID" structs:"userID"`
+	ID     string `bson:"_id" json:"id,omitempty" structs:"id"`
+	UserID string `bson:"userID" json:"userID" structs:"userID"`
+
+	// Type of the integration.
+	// Supported integrations are Slack, Email, PagerDuty.
 	Type       string `bson:"type" json:"type" structs:"type"`
 	Email      string `bson:"email" json:"email" structs:"email"`
 	WebhookURL string `bson:"webhookURL" json:"webhookURL" structs:"webhookURL"`
+
+	// PDRoutingKey is the routing key generated from PD integration.
+	PDRoutingKey string `bson:"pdRoutingKey" json:"pdRoutingKey" structs:"pdRoutingKey"`
+
+	// PDAction is the PD event action. Possible values are trigger, acknowledge and resolve.
+	PDAction string `bson:"pdAction" json:"pdAction" structs:"pdAction"`
+
+	// PDSeverity can be one of info, warning, error, critical, or unknown.
+	PDSeverity string `bson:"pdSeverity" json:"pdSeverity" structs:"pdSeverity"`
+}
+
+// SendPagerDutyEvent sends an event v2 to pagerduty
+func (integration *Integration) SendPagerDutyEvent(timestamp string) error {
+	if integration.PDRoutingKey == "" {
+		log.Infof("Invalid integration. PDRoutingKey not found.")
+
+		return errors.New("invalid integration. PDRoutingKey not found")
+	}
+
+	payload := pagerduty.V2Payload{
+		Summary:   "Site down",
+		Source:    "Uptime",
+		Severity:  integration.PDSeverity,
+		Timestamp: timestamp,
+	}
+	event := pagerduty.V2Event{
+		Payload:    &payload,
+		RoutingKey: integration.PDRoutingKey,
+		Action:     integration.PDAction,
+	}
+
+	_, err := pagerduty.ManageEvent(event)
+
+	if err != nil {
+		log.Infof("Pagerduty event send failed for integration %s", integration.ID)
+	}
+
+	return nil
 }
--- internal/forms/integration.go
+++ internal/forms/integration.go
@@ -7,6 +7,15 @@ type IntegrationForm struct {
 	Type       string `bson:"type" json:"type"`
 	Email      string `bson:"email" json:"email"`
 	WebhookURL string `bson:"webhookURL" json:"webhookURL"`
+
+	// PDRoutingKey is the routing key generated from PD integration.
+	PDRoutingKey string `bson:"pdRoutingKey" json:"pdRoutingKey,omitempty"`
+
+	// PDAction is the PD event action. Possible values are trigger, acknowledge and resolve.
+	PDAction string `bson:"pdAction" json:"pdAction,omitempty" structs:"pdAction"`
+
+	// PDSeverity can be one of info, warning, error, critical, or unknown.
+	PDSeverity string `bson:"pdSeverity" json:"pdSeverity,omitempty"`
 }
 
 // Validate integration form
@@ -17,6 +26,8 @@ func (integrationForm IntegrationForm) Validate() string {
 		return "webhookURL is required for slack integration"
 	} else if integrationForm.Type == "email" && integrationForm.Email == "" {
 		return "email id is required for mail integration"
+	} else if integrationForm.Type == "pagerduty" && (integrationForm.PDRoutingKey == "" || integrationForm.PDAction == "" || integrationForm.PDSeverity == "") {
+		return "pagerduty routing key, action & severity are required"
 	}
 
 	return ""
